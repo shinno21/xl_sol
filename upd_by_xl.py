6d8b6f050603bcb145dd9118f1c646b8ce12ab6d
habashin <habashin.feb1@gmail.com>
2015-07-22 23:54:15 +0900

    複数件ループ処理と更新処理を追加

--- upd_by_xl.py
+++ upd_by_xl.py
@@ -9,10 +9,13 @@ Python3
 
 FILE_NAME = "test.xlsx"
 URL = 'http://127.0.0.1:8000/api/application_inf/'
-HTTP_METHOD = 'POST'
+HTTP_METHOD_POST = 'POST'
+HTTP_METHOD_PUT = 'PUT'
 COLUMNS = [
     'id', 'name', 'r_name', 'classification', 'team', 'summary', 'text', 'user'
 ]
+
+PK_COLUMN = 'id'
 
 CONTENT_TYPE = 'application/json'
 AUTH_PREFIX = 'Token '
@@ -28,25 +31,36 @@ if __name__ == '__main__':
     # 表全体の取得
     rows = ws.rows
 
-    # 初期化 行ごとのリスト
-    table_list = []
+    # http Request
+    http = urllib3.PoolManager()
 
     # 初期化 1行を辞書化
     row_dict = {}
 
-    # 表の内容を辞書のリスト化
+    # 表の内容を全てPOST
+    # 行単位ループ
     for row in rows:
+        # 列単位ループ
         for col_num, col in enumerate(row):
             row_dict[COLUMNS[col_num]] = col.value
-        table_list.append(row_dict)
-        row_dict = {}
-    
-    table = json.dumps(table_list[0], separators=(',', ':'))
-    print(table)
+        # JSON エンコード
+        row_json = json.dumps(row_dict, separators=(',', ':'))
+        print(row_json)
 
-    http = urllib3.PoolManager()
-    r = http.request(HTTP_METHOD, URL,
-        headers={'Content-Type': CONTENT_TYPE,
-                'Authorization': AUTHORIZATION},
-        body=table)
-    print(r.status, r.data)
+        # JSON をPOST して登録
+        # 更新
+        if row_dict[PK_COLUMN]:
+            print(URL + str(row_dict[PK_COLUMN]) + '/' )
+            r = http.request(HTTP_METHOD_PUT, URL + str(row_dict[PK_COLUMN])  + '/'  ,
+                    headers={'Content-Type': CONTENT_TYPE,
+                            'Authorization': AUTHORIZATION},
+                    body=row_json)
+        # 削除
+        else:
+            r = http.request(HTTP_METHOD_POST, URL,
+                    headers={'Content-Type': CONTENT_TYPE,
+                            'Authorization': AUTHORIZATION},
+                    body=row_json)
+        # print(r.status, r.data)
+        print(r.status)
+        row_dict = {}
